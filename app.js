0e801914497035cf576fcfb725a6211cd0a75498
MaximStovba <61358072+MaximStovba@users.noreply.github.com>
2022-06-23 15:50:52 +0300

    Handling Errors

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const { API_ACCESS_KEY, BASE_URL } = process.env;
 const url = `${BASE_URL}/current?access_key=${API_ACCESS_KEY}&query=Moscow`;
 
 request({ url: url, json: true }, (error, response) => {
-  console.log(
-    response.body.current.weather_descriptions[0] +
-      '. It is currently ' +
-      response.body.current.temperature +
-      ' degress out.'
-  );
+  if (error) {
+    console.log('Unable to connect to location services!');
+  } else if (response.body.current === undefined) {
+    console.log('Unable to find location. Try another search.');
+  } else {
+    console.log(
+      response.body.current.weather_descriptions[0] +
+        '. It is currently ' +
+        response.body.current.temperature +
+        ' degress out.'
+    );
+  }
 });
